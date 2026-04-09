f08c4e7632bc21f2a5383d97ed0301bc68892780
Cankutay Mutlu <cankutaymutlu21@gmail.com>
2026-04-09 06:58:50 +0300

    Add LeetCode 704. Binary Search Solution

---
+++ LeetCode/binary_search.py
@@ -0,0 +1,17 @@
+# LeetCode "704. Binary Search" Solution
+
+class Solution(object):
+    def search(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
